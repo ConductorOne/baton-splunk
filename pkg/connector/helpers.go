9a297c08e3216977f54b4b8427376b1bcdf107f8
Martin Albert <martin.albert.187@gmail.com>
2023-08-08 10:29:04 +0200

    Move titleCaser into function

--- pkg/connector/helpers.go
+++ pkg/connector/helpers.go
@@ -13,7 +13,11 @@ import (
 
 const ResourcesPageSize = 50
 
-var titleCaser = cases.Title(language.English)
+func titleCase(s string) string {
+	titleCaser := cases.Title(language.English)
+
+	return titleCaser.String(s)
+}
 
 func annotationsForUserResourceType() annotations.Annotations {
 	annos := annotations.Annotations{}
